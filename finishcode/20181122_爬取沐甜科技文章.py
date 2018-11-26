9e084bb24716eec4eef376bd24bcefe60142e8fa
hunetrgeng <260484499@qq.com>
2018-11-26 22:25:13 +0800

    mac 提交

--- finishcode/20181122_爬取沐甜科技文章.py
+++ finishcode/20181122_爬取沐甜科技文章.py
@@ -9,7 +9,7 @@ def mkdir(path):
     folder = os.path.exists(path)
     if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
         os.makedirs(path)            #makedirs 创建文件时如果路径不存在会创建这个路径
-
+#添加东西
 mixinfo=[]
 count = 0
 def url_deal(aurl):
--- nml.py
+++ nml.py
@@ -10,3 +10,4 @@ np.random.seed(1234)
 d1 = pd.Series(2*np.random.normal(size = 100)+3)
 d2 = np.random.f(2,4,size = 100)
 d3 = np.random.randint(1,100,size = 100)
+#aa
