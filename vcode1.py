43dcc2f5040706d5455dc1b7a104bd31e740907d
hunetrgeng <260484499@qq.com>
2018-11-28 21:16:53 +0800

    mac

--- vcode1.py
+++ vcode1.py
@@ -12,8 +12,7 @@ import matplotlib.pyplot as plt
 import mpl_finance as mpf
 from datetime import datetime
 import matplotlib.gridspec as gridspec
-import talib as tl
-
+import talib
 # 支持中文
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
@@ -21,4 +20,4 @@ plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 df=ut.dailykl('SR1409')
 close=df[4]
 nclose=np.array(close,dtype='f8') #非得做这一步转换，日了狗了
-ma5=tl.MA(nclose,5)
+ma5=talib.MA(nclose,5)
