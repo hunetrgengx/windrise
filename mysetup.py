cb6627a21831226b9acd5d45d9bcd7a4242dfe09
huntergengx <260484499@qq.com>
2018-11-28 16:36:46 +0800

    20181128

--- mysetup.py
+++ mysetup.py
@@ -1,4 +1,19 @@
-# mysetup.py
+# -- coding: UTF-8 --
+#已安装的所有库
 from distutils.core import setup
 import py2exe
+import requests
+import re
+from bs4 import BeautifulSoup
+import time
+import os
+import pandas as pd
+import usuallytool as ut #导入自建函数usuallytool
+import datetime as dt
+import numpy as np
+import random as re
+import matplotlib.pyplot as plt
+import mpl_finance as mpf
+from datetime import datetime
+import matplotlib.gridspec as gri  dspec
 setup(console=['E:\\gj\\code\\vcode1.py'])
--- mytalib.py
+++ mytalib.py
@@ -1,18 +1,10 @@
-import requests
-import re
-from bs4 import BeautifulSoup
-import time
-import os
-import pandas as pd
+# -- coding: UTF-8 --
+import talib
+import numpy as np 
 import usuallytool as ut #导入自建函数usuallytool
-import datetime as dt
-import numpy as np
-import random as re
 import matplotlib.pyplot as plt
-import mpl_finance as mpf
-from datetime import datetime
-import matplotlib.gridspec as gridspec
-import talib as tl
+
+
 
 # 支持中文
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
@@ -21,4 +13,9 @@ plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 df=ut.dailykl('SR1409')
 close=df[4]
 nclose=np.array(close,dtype='f8') #非得做这一步转换，日了狗了
-ma5=tl.MA(nclose,5)
+ma5=talib.MA(nclose,5)
+'''
+from talib import MA_Type #终于懂了
+upper, middle, lower = talib.BBANDS(close, matype=MA_Type.T3)
+upper, middle, lower = talib.BBANDS(close, matype=talib.MA_Type.T3)
+'''
