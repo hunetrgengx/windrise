03be64c18e87c039856163f8ebeb2b42369836f7
hunetrgeng <260484499@qq.com>
2018-11-29 23:10:07 +0800

    a

--- usually1.py
+++ usually1.py
@@ -12,7 +12,8 @@ aopen
 
  for i in range(24,len(upper)):
      compare=upper[i]
-     for i in range(aopen(where aopen.status=1).count())
-     if (compare>aopen.price*1.02 or compare<aopen.price*0.99):
-       self.profit=(compare-aopen.price)*10
-       self.status=0
+     for i in range(aopen(where aopen.status=1).count()):
+      if (compare>aopen.price*1.02 or compare<aopen.price*0.99):
+        self.profit=(compare-aopen.price)*10
+        self.status=0
+      
