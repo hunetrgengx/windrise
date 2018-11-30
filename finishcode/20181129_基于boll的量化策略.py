5a61f45b4e7a5d0449ae68dec75c0f785844947e
huntergengx <260484499@qq.com>
2018-11-30 17:23:05 +0800

    win

--- finishcode/20181129_基于boll的量化策略.py
+++ finishcode/20181129_基于boll的量化策略.py
@@ -4,47 +4,63 @@ import pandas as pd
 import usuallytool as ut 
 import talib 
 import numpy as np 
-
+import matplotlib as mpl
 #定义期货投资者Futures investors
-class fior():
-    def __init__(self,name):
-        self.name=name
-        self.balance=200000
-        self.position=0
-        self.time=10
-        self.direction=0
-        self.price=0
-        self.win=0
-        self.lose=0
 
-#策略：交易者根据收盘进行决定是否开仓，如果收盘价高于boll线上线，则进行建仓，这个仓位盈利百分之10、亏损百分之5平仓
-ming=fior('ming')
+class fopen():
+    def __init__(self):
+            self.price=0
+            self.direction=0
+            self.number=0
+            self.status=0
+            self.profit=0
+            self.closing=0
+
+
 df=ut.dailykl('SR0')
 close=df[4]
 nclose=np.array(close,dtype='f8') #非得做这一步转换，日了狗了
 upper, middle, lower = talib.BBANDS(nclose, matype=talib.MA_Type.T3)
+close=nclose
 
+position=[]
+over=[]
+mid=[]
+num=0
+for i in range(25,len(close)):
+    #判断买入价及是否买入
+    if close[i]<lower[i]:
+        a=fopen()
+        a.price=close[i]
+        a.direction=1
+        a.number=i
+        a.status=1
+        position.append(a)
+    #判断卖出的问题
+    if len(position)==0:
+            c=1
+    elif len(position)>0:
+        for j in range(len(position)):
+            if (close[i]>position[j].price*1.02 or close[i]<position[j].price*0.99):
+                position[j].status=0
+                position[j].closing=close[i]
+                position[j].profit=(close[i]-position[j].price)*10
 
+len(position)  
+over=[] 
+win=[]
+lose=[]
+winprofit=0
+loseprofit=0
+for i in range(len(position)):          
+    if position[i].status==0:
+        over.append(position[i])
 
-print(len(upper))
-for j in range(150):     
-    for i in range(ming.time+1,len(upper)):
-        if upper[i]<nclose[i]:
-            ming.position+=1
-            ming.balance-=nclose[i]
-            ming.time=i
-            break    
-    
-    for i in range(ming.time,len(upper)):
-        compareprice=nclose[i]
-        lose=nclose[ming.time]*1.1
-        win=nclose[ming.time]*0.8
-        if nclose[i]>lose:
-            ming.balance+=(nclose[ming.time]-(nclose[i])*10+nclose[ming.time])
-            ming.lose+=1
-            break
-        elif nclose[i]<win:
-            ming.balance+=((nclose[ming.time]-nclose[i])*10+nclose[ming.time])
-            ming.win+=1
-            break    
-    print(j,ming.balance,i,'平仓价',nclose[i],'开仓价',nclose[ming.time],ming.win,ming.lose)
+for i in range(len(position)):
+    if position[i].profit>0:
+        win.append(position[i])
+        winprofit+=position[i].profit
+    elif position[i].profit<0:
+        lose.append(position[i])
+        loseprofit+=position[i].profit
+#策略：交易者根据收盘进行决定是否开仓，如果收盘价高于10日均线，则进行建仓，这个仓位盈利百分之10、亏损百分之5平仓
--- vcode1.py
+++ vcode1.py
@@ -4,90 +4,73 @@ import pandas as pd
 import usuallytool as ut 
 import talib 
 import numpy as np 
-
+import matplotlib as mpl
 #定义期货投资者Futures investors
-class fior():
-    def __init__(self,name):
-        self.name=name
-        self.balance=200000
-        self.position=0
-        self.time=10
-        self.direction=0
-        self.price=0
-        self.win=0
-        self.lose=0
 
-#策略：交易者根据收盘进行决定是否开仓，如果收盘价高于10日均线，则进行建仓，这个仓位盈利百分之10、亏损百分之5平仓
-ming=fior('ming')
+class fopen():
+    def __init__(self):
+            self.price=0
+            self.direction=0
+            self.number=0
+            self.status=0
+            self.profit=0
+            self.closing=0
+
+
 df=ut.dailykl('SR0')
 close=df[4]
 nclose=np.array(close,dtype='f8') #非得做这一步转换，日了狗了
 upper, middle, lower = talib.BBANDS(nclose, matype=talib.MA_Type.T3)
+close=nclose
 
+position=[]
+over=[]
+mid=[]
+num=0
+for i in range(25,len(close)):
+    #判断买入价及是否买入
+    if close[i]<lower[i]:
+        a=fopen()
+        a.price=close[i]
+        a.direction=1
+        a.number=i
+        a.status=1
+        position.append(a)
+    #判断卖出的问题
+    if len(position)==0:
+            c=1
+    elif len(position)>0:
+        for j in range(len(position)):
+            if close[i]>position[j].price*1.02:
+                position[j].status=0
+                position[j].closing=position[j].price*1.02
+                position[j].profit=position[j].price*0.02*10
+            elif close[i]<position[j].price*0.98:
+                position[j].status=0
+                position[j].closing=position[j].price*0.98
+                position[j].profit=-position[j].price*0.02*10
 
+len(position)  
+over=[] 
+win=[]
+lose=[]
+winprofit=0
+loseprofit=0
+for i in range(len(position)):          
+    if position[i].status==0:
+        over.append(position[i])
 
+for i in range(len(position)):
+    if position[i].profit>0:
+        win.append(position[i])
+        winprofit+=position[i].profit
+    elif position[i].profit<0:
+        lose.append(position[i])
+        loseprofit+=position[i].profit
 
-for i in range(ming.time+1,len(upper)):
-    if upper[i]<nclose[i]:
-        ming.position+=1
-        ming.balance-=nclose[i]
-        ming.time=i
-        break    
-
-for i in range(ming.time,len(upper)):
-    compareprice=nclose[i]
-    lose=nclose[ming.time]*1.05
-    win=nclose[ming.time]*0.9
-    if nclose[i]>lose:
-        ming.balance+=(nclose[ming.time]-(nclose[i])*(-10)+nclose[ming.time])
-        ming.lose+=1
-        break
-    elif nclose[i]<win:
-        ming.balance+=((nclose[ming.time]-nclose[i])*(-10)+nclose[ming.time])
-        ming.win+=1
-        break    
-print(,ming.balance,i,'平仓价',nclose[i],'开仓价',nclose[ming.time],ming.win,ming.lose)
-
-
-for i in range(ming.time+1,len(upper)):
-    if upper[i]<nclose[i]:
-        ming.position+=1
-        ming.balance-=nclose[i]
-        ming.time=i
-        break    
-
-for i in range(ming.time,len(upper)):
-    compareprice=nclose[i]
-    lose=nclose[ming.time]*1.05
-    win=nclose[ming.time]*0.9
-    if nclose[i]>lose:
-        ming.balance+=(nclose[ming.time]-(nclose[i])*(-10)+nclose[ming.time])
-        ming.lose+=1
-        break
-    elif nclose[i]<win:
-        ming.balance+=((nclose[ming.time]-nclose[i])*(-10)+nclose[ming.time])
-        ming.win+=1
-        break    
-print(2,ming.balance,i,'平仓价',nclose[i],'开仓价',nclose[ming.time],ming.win,ming.lose)
-
-
-for i in range(ming.time+1,len(upper)):
-    if upper[i]<nclose[i]:
-        ming.position+=1
-        ming.balance-=nclose[i]
-        ming.time=i
-        break    
-
-for i in range(ming.time,len(upper)):
-    compareprice=nclose[i]
-    lose=nclose[ming.time]*1.05
-    win=nclose[ming.time]*0.9
-    if nclose[i]>lose:
-        ming.balance+=(nclose[ming.time]-(nclose[i])*(-10)+nclose[ming.time])
-        ming.lose+=1
-        break
-    elif nclose[i]<win:
-        ming.balance+=((nclose[ming.time]-nclose[i])*(-10)+nclose[ming.time])
-        ming.win+=1
-        break    
-print(3,ming.balance,i,'平仓价',nclose[i],'开仓价',nclose[ming.time],ming.win,ming.lose)
+result=[]
+for i in range(len(position)):
+        result.append([position[i].price,position[i].closing])
+result=pd.DataFrame(data=result)
+result.to_excel('a.xls')
+#策略：交易者根据收盘进行决定是否开仓，如果收盘价高于10日均线，则进行建仓，这个仓位盈利百分之10、亏损百分之5平仓
